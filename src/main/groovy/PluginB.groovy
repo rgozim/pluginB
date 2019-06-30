1e2596d7729b7df3edb7d12f6ff23b82f50740d2
Riad Gozim <rgozim1@gmail.com>
2019-06-30 15:15:01 +0100

    first commit

---
+++ src/main/groovy/PluginB.groovy
@@ -0,0 +1,13 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class PluginB implements Plugin<Project> {
+
+    private Project project
+
+    @Override
+    void apply(Project project) {
+        this.project = project
+    }
+
+}
